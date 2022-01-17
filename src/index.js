98f7f951f4dd7f8b2805b6b68e31126c9ce4b78d
andres-condezo <ra.condezomonge@gmail.com>
2022-01-17 16:48:58 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// *****************
+// Imports
+// *****************
+
+import './styles/style.css';
+import ToDoApp from './modules/app.js';
+import { $ } from './modules/utils.js';
+
+// ***************
+//  Main Function
+// ***************
+
+const app = new ToDoApp();
+const $newTaskInput = $('.new-task');
+const $refreshBtn = $('.refresh');
+const $removeCompletedBtn = $('.removeCompletedBtn');
+
+const main = () => {
+  app.getLocalStorage();
+  app.displayTasks();
+  $refreshBtn.addEventListener('click', () => {
+    app.clearTaskArr();
+    app.ChangeClearBtnState();
+    app.displayTasks();
+  });
+  $newTaskInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') { app.addTask($newTaskInput); }
+  });
+  $removeCompletedBtn.addEventListener('click', () => {
+    app.deleteCompletedTasks();
+  });
+  app.ChangeClearBtnState();
+};
+
+main();
